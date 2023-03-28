dc61123f072d65d8cb2973bd3fb7582392f7fa2d
skrowll <lucasdejesus.rocha@hotmail.com>
2023-03-27 22:20:50 -0300

    feat: undo

--- src/services/musicsAPI.js
+++ src/services/musicsAPI.js
@@ -1,10 +1,5 @@
 const getMusics = async (id) => {
-  const request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`, {
-    mode: "no-cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`);
   const requestJson = await request.json();
   return requestJson.results;
 };
