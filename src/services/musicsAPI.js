8476e298124571144d09de2ce97b813ac07dca57
skrowll <lucasdejesus.rocha@hotmail.com>
2023-03-27 21:47:54 -0300

    feat: add mode and headers to fetch

--- src/services/musicsAPI.js
+++ src/services/musicsAPI.js
@@ -1,5 +1,10 @@
 const getMusics = async (id) => {
-  const request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`);
+  const request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`, {
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
   const requestJson = await request.json();
   return requestJson.results;
 };
